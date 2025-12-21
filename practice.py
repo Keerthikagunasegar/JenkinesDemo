e5f038cea6eedfb9ebb0a052fdabb95dcf2b240c
Keerthikagunasegar <keerthikagunasegar1101@gmail.com>
2025-12-21 12:00:02 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -2,8 +2,10 @@ name = "Abhi"
 age = 21
 place = "Bangalore"
 gender = "Female"
+DOB = 11-01-2004
 
 print("My name is", name)
 print("My age is", age)
 print("My place is", place)
 print("My gender is", gender)
+print("My DOB is", DOB)
